a0cf0f064981bbd9619ea1e9653a852d70bf7579
harrisyoung27 <138505736+harrisyoung27@users.noreply.github.com>
2024-12-22 17:12:54 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,56 @@
+let reaction = document.querySelector("#reaction");
+let [miliseconds, seconds] = [0, 0];
+timer = null;
+milisecondTimer = null;
+let random;
+
+function miliSeconds() {
+    miliseconds++;
+}
+
+function greenBackground(){
+    reaction.style.background = "green";
+    reaction.innerHTML = "Click now!!";
+    if (milisecondTimer !== null) {
+        clearInterval(milisecondTimer);
+    }
+    milisecondTimer = setInterval(miliSeconds, 1);
+}
+
+function randomizer() {
+    if (timer !== null) {
+        clearInterval(timer);
+    }
+    random = Math.floor(Math.random() * 3000 + 5000);
+    timer = setInterval(greenBackground, random);
+}
+
+reaction.addEventListener("click", event => {
+    if (reaction.style.background == "green") {
+        reaction.innerHTML = "Reaction Speed: " + miliseconds + "ms";
+        reaction.style.background = "white";
+        reaction.style.color = "green";
+        clearInterval(timer);
+        clearInterval(milisecondTimer);
+    } else {
+        reaction.innerHTML = "Too Fast!! Try Again and Re-Hover your cursor here.";
+        reaction.style.background = "white";
+        reaction.style.color = "black";
+        clearInterval(timer);
+        clearInterval(milisecondTimer);
+    }
+})
+
+reaction.addEventListener("mouseover", event => {
+    randomizer();
+    reaction.style.background = "red";
+    reaction.innerHTML = "Click when turned Green..";
+})
+
+reaction.addEventListener("mouseout", event => {
+    clearInterval(timer);
+    clearInterval(milisecondTimer);
+    reaction.style.background = "white";
+    reaction.innerHTML = "Try Again? Hover Here!!";
+    reaction.style.color = "black";
+})
